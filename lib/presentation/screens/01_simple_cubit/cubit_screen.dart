f578f5d67f9fa02099efb9f271611249c214a131
franco galbiati <francogalbiati984@gmail.com>
2024-09-19 19:55:54 -0300

    add cubit name example

--- lib/presentation/screens/01_simple_cubit/cubit_screen.dart
+++ lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -1,18 +1,31 @@
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitScreen extends StatelessWidget {
   const CubitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final usernameCubit = context.watch<UsernameCubit>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          usernameCubit.setUsername(
+            RandomGenerator.getRandomName(),
+          );
+        },
+        child: const Icon(Icons.refresh),
       ),
+      body: BlocBuilder<UsernameCubit, String>(builder: (context, state) {
+        return Center(
+          child: Text(state),
+        );
+      }),
     );
   }
 }
